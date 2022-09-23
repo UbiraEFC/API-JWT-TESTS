ef5fd96f9ddc99ee197cb496c292d5ec87875e75
UbiraEFC <ubirata.efc@gmail.com>
2022-09-23 18:16:25 -0300

    Add Swagger Documentation

--- src/infra/app.ts
+++ src/infra/app.ts
@@ -1,14 +1,18 @@
 import "reflect-metadata";
 import "express-async-errors";
+import swaggerUi from "swagger-ui-express";
 
 import express, { NextFunction, Request, Response }  from "express";
 import { AppError } from "../errors/AppError";
 
 import { router } from "./routes";
+import swaggerFile from "./swagger.json";
 
 export const app = express();
 
 app.use(express.json());
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use(router);
 
